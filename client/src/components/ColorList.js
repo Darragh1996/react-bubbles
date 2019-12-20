dd75324efb7f59468f7ff97aae795aabc150e229
Darragh Ferry <darraghferry@hotmail.com>
2019-12-20 09:23:06 +0000

    delete functionality now works

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../axios/helper";
 
 const initialColor = {
   color: "",
@@ -25,6 +25,19 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log(res);
+        updateColors(
+          colors.filter(c => {
+            return c.id !== color.id;
+          })
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
@@ -34,12 +47,14 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
